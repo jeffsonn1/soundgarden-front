c61dfda037ac60abf7df3a25d5f75e42afa53937
jeffsonn1 <paulojefferson1972@gmail.com>
2023-04-11 19:41:41 -0300

    modifiquei o cadastro de eventos

--- js/cadastro-evento.js
+++ js/cadastro-evento.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+
 
 let enviarButton = document.getElementById('enviar');
 let nomeInput = document.getElementById('nome');
@@ -14,36 +14,39 @@ enviarButton.onclick = function(event){
         event.preventDefault(); // Impede o envio do formulário
         lnome_msg.style.display = 'block';
     } else {
-    event.preventDefault();
-    var dataISO = new Date(dataInput.value).toISOString();
-    var requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({   
-            "name": nomeInput.value,
-            "poster": "link da imagem",
-            "attractions": [
-                atracaoInput.value,
-            ],
-            "description": descricaoInput.value,
-            "scheduled": dataISO,
-            "number_tickets": lotacaoInput.value,
-        }),
-    };
-      
-    fetch("https://soundgarden-api.vercel.app/events", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-    //limpar campos após o envio
-    nomeInput.value = "";
-    atracaoInput.value = "";
-    descricaoInput.value = "";
-    dataInput.value = "";
-    lotacaoInput.value = "";
+        event.preventDefault();
+
+        // Converte a data para o formato ISO 8601
+        var dataFormatada = new Date(dataInput.value).toISOString();
+
+        var requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({   
+                "name": nomeInput.value,
+                "poster": "link da imagem",
+                "attractions": [
+                    atracaoInput.value,
+                ],
+                "description": descricaoInput.value,
+                "scheduled": dataFormatada, // Usa a data formatada em ISO 8601
+                "number_tickets": lotacaoInput.value,
+            }),
+        };
+          
+        fetch("https://soundgarden-api.vercel.app/events", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        
+        // Limpa os campos após o envio
+        nomeInput.value = "";
+        atracaoInput.value = "";
+        descricaoInput.value = "";
+        dataInput.value = "";
+        lotacaoInput.value = "";
     }
 }
 
-});
